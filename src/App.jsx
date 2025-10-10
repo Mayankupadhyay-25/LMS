ffc87e56dedeb33f4b8ab160d04f0d30ad9c6073
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-10-10 23:51:51 +0530

    setroutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Routes>
+        <Route path="/" element={<Hom/>} />
+        <Route path="/about" element={<h1>About Page</h1>} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
