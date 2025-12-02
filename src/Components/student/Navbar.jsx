2a3821bcbe07de0ff1141106582678bd81a3306c
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-12-03 00:54:00 +0530

    mobileButtonClick

--- src/Components/student/Navbar.jsx
+++ src/Components/student/Navbar.jsx
@@ -22,24 +22,36 @@ const Navbar = () => {
       />
       <div className="hidden md:flex item-center gap-5 text-gap-5 text-gray-500">
         <div className="flex items-center gap-5">
-          <button>Become Educator</button>
-          <Link to="/my-enrollments">My Enrollment </Link>
+          {user &&
+          <>
+          <button> Bccome Educator</button>
+          <Link to ="/my-enrollments">My Enrollments</Link>
+          </>}
         </div>
+
        { user ? <UserButton/>:
-        
+
         <button onClick={()=> openSignIn()} className="bg-blue-600 text-white px-5 py-2 rounded-full">
           Create Account
         </button>}
       </div>
       {/* For phone view?? */}
       <div className="m d:hidden flex items-center gap-2 sm:gap-5 taxt-gray-500">
-        <div>
-          <button>Become Educator</button>
-          <Link to="/my-enrollments">My Enrollment </Link>
+        <div className="flex items-center gap-1 sm:gap-2 max-sm:text-xs">
+          {user &&
+          <>
+          <button> Bccome Educator</button>
+          <Link to ="/my-enrollments">My Enrollments</Link>
+          </>}
+
         </div>
-        <button>
+        {
+          user ? <UserButton/>:
+          <button onClick={()=> openSignIn()}>
           <img src={assets.user_icon} alt="" />
         </button>
+          }
+        
       </div>
     </div>
   );
