45c368faee158bf6cd40deb5f567ee940e87ebcb
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-12-21 21:09:50 +0530

    searchbarComplate

--- src/pages/student/Home.jsx
+++ src/pages/student/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../../Components/student/Hero'
+import SearchBar from '../../Components/student/SearchBar'
 
 const Home = () => {
   return (
     <div className='flex flex-col items-center space-y-7 text-center'>
         <Hero/>
+        <SearchBar/>
     </div>
   )
 }
