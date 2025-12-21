315032bd5d19e8612b56454587092cd3b50266a8
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-12-22 00:33:17 +0530

    companies

--- src/pages/student/Home.jsx
+++ src/pages/student/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../../Components/student/Hero'
 import SearchBar from '../../Components/student/SearchBar'
+import Companies from '../../Components/student/Companies'
 
 const Home = () => {
   return (
     <div className='flex flex-col items-center space-y-7 text-center'>
         <Hero/>
         <SearchBar/>
+         <Companies/>
     </div>
   )
 }
