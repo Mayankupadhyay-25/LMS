cfcbf5d694bd8750b1fc1b42c0bd6f495d0d7003
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2026-01-07 19:52:59 +0530

    codeInSeqeence

--- src/Components/student/Footer.jsx
+++ src/Components/student/Footer.jsx
@@ -1,39 +1,62 @@
-import React from 'react'
-import { assets } from '../../assets/assets'
+import React from "react";
+import { assets } from "../../assets/assets";
 
 const Footer = () => {
   return (
-    <footer className='bg-gray-900 md:px-36 text-left w-full'>
-      <div className='flex flex-col md:flex-row items-start px-8 md:px-0 
-          justify-center gap-32 py-10 border-b border-white/30'>
-        <div className='flex flex-col md:items-start items-center'>
+    <footer className="bg-gray-900 md:px-36 text-left w-full">
+      <div
+        className="flex flex-col md:flex-row items-start px-8 md:px-0 
+          justify-center gap-32 py-10 border-b border-white/30"
+      >
+        <div className="flex flex-col md:items-start items-center">
           <img src={assets.logo_dark} alt="Logo" />
-          <p className='mt-6 text-center md:text-left text-sm text-white/80'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos 
-            necessitatibus dicta modi blanditiis</p>
+          <p className="mt-6 text-center md:text-left text-sm text-white/80">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
+            necessitatibus dicta modi blanditiis
+          </p>
         </div>
-        <div className='flex flex-col items-center w-full'>
-          <h2 className='font-semibold text-white mb-5 text-center'>Company</h2>
-          <ul className='flex flex-col w-full justify-center items-center text-sm text-white/80 md:space-y-2'>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Contact Us</a></li>
-            <li><a href="#">Privacy Policy</a></li>
+        <div className="flex flex-col items-center w-full">
+          <h2 className="font-semibold text-white mb-5 text-center">Company</h2>
+          <ul className="flex flex-col w-full justify-center items-center text-sm text-white/80 md:space-y-2">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About Us</a>
+            </li>
+            <li>
+              <a href="#">Contact Us</a>
+            </li>
+            <li>
+              <a href="#">Privacy Policy</a>
+            </li>
           </ul>
         </div>
-        <div className='hidden md:flex flex-col items-start w-full'>
-          <h2 className='font-semibold text-white mb-5'>Subsscribe to our newsletter</h2>
-          <p className='text-sm text-white/80'>The latest news, articals, and 
-          resources sent to your inbox weekly.</p>
-          <div className='flex items-center gap-2 pt-4'>
-            <input type="email" placeholder="Enter your email" className='border-gray-500/30 bg-gray-800
-            text-gray-500 placeholder-gray-500 outline-none w-64 h-9 rounded px-2 text-sm'></input>
-            <button className='bg-blue-600 w-24 h-9 text-white rounded'>Subscribe</button>
+        <div className="hidden md:flex flex-col items-start w-full">
+          <h2 className="font-semibold text-white mb-5">
+            Subsscribe to our newsletter
+          </h2>
+          <p className="text-sm text-white/80">
+            The latest news, articals, and resources sent to your inbox weekly.
+          </p>
+          <div className="flex items-center gap-2 pt-4">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="border-gray-500/30 bg-gray-800
+            text-gray-500 placeholder-gray-500 outline-none w-64 h-9 rounded px-2 text-sm"
+            ></input>
+            <button className="bg-blue-600 w-24 h-9 text-white rounded">
+              Subscribe
+            </button>
           </div>
         </div>
       </div>
-      <p className='py-4 text-center text-xs md:text-sm text-white/60'>Copyright 2025 © GreatStack. All Right Reserved</p>
+      <p className="py-4 text-center text-xs md:text-sm text-white/60">
+        Copyright 2025 © GreatStack. All Right Reserved
+      </p>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
