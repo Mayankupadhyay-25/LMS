e72d08bde2cbe4761d9b68b01396464e442aceb9
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2026-02-04 00:32:28 +0530

    refactor: simplify Educator component structure

--- src/pages/educator/educator.jsx
+++ src/pages/educator/educator.jsx
@@ -1,14 +1,24 @@
+// import React from 'react'
+// import { Outlet } from 'react-router-dom'
+
+// const Educator = () => {
+//   return (
+//     <div>
+//         <h1>educator</h1>
+//         <div>
+//           {<Outlet/>}
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Educator
+
 import React from 'react'
-import { Outlet } from 'react-router-dom'
 
 const Educator = () => {
   return (
-    <div>
-        <h1>educator</h1>
-        <div>
-          {<Outlet/>}
-        </div>
-    </div>
+    <div>Educator</div>
   )
 }
 
